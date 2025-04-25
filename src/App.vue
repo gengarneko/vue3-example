871bd5b2e86ac018ae2fec54ac38ce4fc0ec44ae
gengarneko <gengarneko@gmail.com>
2025-04-25 11:52:51 +0800

    feat(ui): use shadcn/vue as ui lib

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
+import { Button } from "@/components/ui/button";
+const links: { name: string; href: string }[] = [
+	{ name: "List", href: "/list" },
+	{ name: "Canvas", href: "/canvas" },
+	{ name: "Three.js", href: "/three" },
+];
 </script>
 
 <template>
   <div class="h-screen w-screen flex flex-col">
     <nav class="h-16 border-b border-gray-300">
-      <div class="h-full max-w-7xl m-auto flex gap-4 items-center px-2">
-        <router-link to="/list">List</router-link>
-        <router-link to="/canvas">Canvas</router-link>
-        <router-link to="/three">Three.js</router-link>
+      <div class="h-full max-w-7xl m-auto flex gap-4 items-center">
+        <router-link v-for="link in links" :key="link.name" :to="link.href">
+          <Button variant="ghost" class="hover:cursor-pointer">{{ link.name }}</Button>
+        </router-link>
       </div>
     </nav>
 
